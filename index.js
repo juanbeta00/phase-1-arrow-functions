e2f48c73c06f5f04b57cf5e023aa62194c0719a3
JUAN <juanbeta00@hotmail.com>
2022-04-28 17:11:35 -0400

    commit tree 8eaedf62196bc8b4c939cfa857c4e6152b080f45

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function (num1 = 2000, num2 = 100) {
+    return num1 / num2;
+}
+
+let square = (x) => x * x;
+
+let add = (numero1, numero2) => numero1 + numero2;
+
+console.log(divide);
+
+console.log(square(2));
+
+console.log(add(2,2));
